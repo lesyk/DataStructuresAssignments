eaeba1fb8b5d1be3ab2a7347108f7130f4113602
Viktor Lesyk <vee.lesyk@gmail.com>
2016-10-20 09:31:55 +0200

    Add: solutions for two assignments

---
+++ Week 1/check_brackets_in_code/check_brackets_in_code.ts
@@ -0,0 +1,86 @@
+class Bracket {
+  bracketType: string;
+  position: number;
+
+  constructor(bracketType: string, position: number) {
+    this.bracketType = bracketType;
+    this.position = position;
+  }
+
+  match(char:string) {
+    if (this.bracketType === '[' && char === ']') {
+      return true;
+    }
+    else if (this.bracketType === '{' && char === '}') {
+      return true;
+    }
+    else if (this.bracketType === '(' && char === ')') {
+      return true;
+    }
+    return false;
+  }
+
+  static isOpened(char: string) {
+    return (['[', '{', '('].indexOf(char) !== -1);
+  }
+
+  static isClosed(char: string) {
+    return ([']', '}', ')'].indexOf(char) !== -1);
+  }
+}
+
+function matchBrackets(inputText: string): string {
+  if (!inputText) {
+    return "Empty String";
+  }
+
+  let bracketsStack: Array<Bracket> = [];
+
+  for (let i = 0; i < inputText.length; i++) {
+    if (Bracket.isOpened(inputText[i])) {
+      bracketsStack.push(new Bracket(inputText[i], i));
+    }
+    else {
+      if (bracketsStack.length === 0 &&
+        (Bracket.isOpened(inputText[i]) || Bracket.isClosed(inputText[i]))) {
+        return `${i + 1}`;
+      }
+      else if (Bracket.isClosed(inputText[i])) {
+        let lastInStack = bracketsStack.pop();
+        if (!lastInStack.match(inputText[i]) && !/^[^\[\{\(\]\}\)]*$/.test(inputText[i])) {
+            return `${i + 1}`;
+        }
+      }
+    }
+  }
+  if (bracketsStack.length === 0) {
+    return 'Success';
+  }
+  else {
+    let lastInStack = bracketsStack.pop();
+    return `${lastInStack.position + 1}`;
+  }
+}
+
+// let readline = require('readline');
+// let rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//     terminal: false
+// });
+
+// rl.on('line', function(line){
+//     console.log(matchBrackets(line));
+//     process.exit();
+// })
+
+const testFolder = './tests/';
+const fs = require('fs');
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    fs.readFile(testFolder+file, 'utf8', (err, data) => {
+      if (err) throw err;
+      console.log(matchBrackets(data));
+    });
+  });
+})
---
+++ Week 1/tree_height/tree_height.ts
@@ -0,0 +1,110 @@
+class Tree {
+	tree: Array<number>;
+	maxDepth = 0;
+
+	constructor(line: string) {
+		this.tree = line.split(' ').map(function(el) { return parseInt(el, 10); });
+	}
+
+	getTreeHeight():number {
+    let depths = [];
+    let currDepth = 1;
+
+    for (let i = 0; i < this.tree.length; i++) {
+			currDepth = 1;
+
+			let index = i;
+			while (index !== -1) {
+				if ((index = this.tree[index]) === -1) {
+					break;
+				}
+					
+				//take cached 
+				if (depths[index]) {
+					currDepth += depths[index];
+					index = -1;
+				} else {
+					currDepth++;
+				}
+
+				if (currDepth > this.maxDepth) {
+					this.maxDepth = currDepth;
+				}
+			}
+
+			depths.push(currDepth);
+    }
+
+    return this.maxDepth;
+	}
+} 
+
+// Alternative method, with higher O()
+// class Tree {
+// 		n: number;
+// 		parent: Array<string>;
+// 		vertices: Array<string>;
+// 		height = 1;
+
+// 		constructor(n: number) {
+// 			this.n = n;
+// 		}
+
+// 		addToStack(parent: string, depth: number):number {
+// 			let d = depth + 1;
+// 			if (d > this.height) {
+// 				this.height = d;
+// 			}
+// 			// console.log("depth",d);
+// 			for(let i = 0; i < this.vertices.length;i++) {
+// 				if(this.vertices[i] == parent){
+// 					this.parent.push(i.toString());
+// 					this.addToStack(i, d);
+// 				}
+// 			}
+// 			return d;
+// 		}
+
+// 		computeHeight(line: string):number {
+// 			let maxDepth = this.n;
+// 			this.vertices = line.split(" ");
+// 			const root = this.vertices.indexOf("-1").toString();
+// 			this.parent = [root];
+// 			this.addToStack(root.toString(), 0)
+
+// 			return this.height;
+// 		}
+// }
+
+let readline = require('readline');
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+// let lineNumber = 1;
+// rl.on('line', function(line){
+// 	if(lineNumber === 1){
+// 		tree = new TreeHeight(parseInt(line));
+// 	}
+// 	if(lineNumber === 2){
+// 		console.log(tree.computeHeight(line));
+// 		process.exit();
+// 	}
+// 	lineNumber++;
+// })
+
+const testFolder = './tests/';
+const fs = require('fs');
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+		if(file.indexOf(".a")=== -1){
+			fs.readFile(testFolder+file, 'utf8', (err, data) => {
+				let lines = data.split("\r\n");
+				let tree = new Tree(lines[1]);
+				console.log(`${tree.getTreeHeight()}`);
+			});
+		}
+  });
+})
